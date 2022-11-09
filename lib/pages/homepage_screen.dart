32223e4b51580b5144996e4e8876e56a0a21a91b
oakarakum <onur.ardakarakum@outlook.com>
2022-11-09 10:31:29 +0300

    sdfs

--- lib/pages/homepage_screen.dart
+++ lib/pages/homepage_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:hava_durumu/features/topbar.dart';
 
 class HomePageScreen extends StatefulWidget {
   const HomePageScreen({super.key});
@@ -12,6 +13,17 @@ class HomePageScreen extends StatefulWidget {
 class _HomePageScreenState extends State<HomePageScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        child: Column(
+          children: [SizedBox(height: 20), Topbar()],
+        ),
+      ),
+    );
   }
 }

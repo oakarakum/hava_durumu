8b8d9d326c002faaf35f37779aa0a275ca6b168c
oakarakum <onur.ardakarakum@outlook.com>
2022-11-09 09:39:29 +0300

    development ayarlandı.

---
+++ lib/pages/weather_detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class WeatherDetalScreen extends StatefulWidget {
+  const WeatherDetalScreen({super.key});
+
+  @override
+  State<WeatherDetalScreen> createState() => _WeatherDetalScreenState();
+}
+
+class _WeatherDetalScreenState extends State<WeatherDetalScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
